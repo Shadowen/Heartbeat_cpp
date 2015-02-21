a4a94afb64b8ab2a2fd4ce2a86f555928722678f
wesleyOnLucy <wesley@heungs.com>
2015-02-21 16:58:35 -0500

    Initial commit

---
+++ Heartbeat.cpp
@@ -0,0 +1,91 @@
+#include "Heartbeat.h"
+
+// Singleton object
+Heartbeat_class Heartbeat;
+
+Heartbeat_class::Heartbeat_class():
+	serialState(WAIT_IDENTIFIER)
+{
+	Serial.begin(9600);
+}
+
+Heartbeat_class::~Heartbeat_class()
+{
+}
+
+void Heartbeat_class::begin(void (*callback)(byte, byte, void*))
+{
+	parseData_callback = callback;
+}
+
+void serialEvent(){
+	Heartbeat.checkSerial();
+}
+
+void Heartbeat_class::checkSerial(){
+  if (Serial.available() > 0){
+    switch(serialState){
+    case WAIT_IDENTIFIER:
+      // Recieving a new transmission
+      identifier = Serial.read();
+      sendMonitor("ID: " + String(identifier));
+    case WAIT_LENGTH:
+      bytesExpected = Serial.read();
+      // Did not find bytesExpected
+      if (bytesExpected == -1){
+        serialState = WAIT_LENGTH;
+        break;
+      }
+      sendMonitor("BytesExpected: " + String(bytesExpected));
+      // Prepare for reading the message
+      bytesRecieved = 0;
+    case WAIT_MESSAGE:
+      // Read the message
+      bytesRecieved += Serial.readBytes(serialBuffer + bytesRecieved, bytesExpected - bytesRecieved);
+      // Message is incomplete
+      if (bytesRecieved != bytesExpected){
+        serialState = WAIT_MESSAGE;
+        break;
+      }
+      // Message is complete
+	  if (parseData_callback != NULL)
+	  {
+		parseData_callback(identifier, bytesRecieved, serialBuffer);
+      }
+	  serialState = WAIT_IDENTIFIER;
+      break;
+    default:
+      // Error
+      sendMonitor("Invalid serial state!");
+    }
+  }
+}
+
+void Heartbeat_class::sendMonitor(String text){
+  sendString(0, text);
+}
+
+void Heartbeat_class::sendHeartbeat(){
+  Serial.write(0);
+  Serial.write(0);
+}
+
+void Heartbeat_class::sendByte(const byte& id, const byte& data)
+{
+  Serial.write(id);
+  Serial.write(1);
+  Serial.write(data);	
+}
+
+void Heartbeat_class::sendInt(const byte& id, const int& data)
+{
+  Serial.write(id);
+  Serial.write(4);
+  Serial.write(data);
+}
+
+void Heartbeat_class::sendString(const byte& id, const String& data){
+  Serial.write(id);
+  Serial.write(data.length());
+  Serial.print(data.c_str());
+}
---
+++ Heartbeat.h
@@ -0,0 +1,42 @@
+#ifndef HEARTBEAT
+#define HEARTBEAT
+
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+
+class Heartbeat_class
+{
+private:
+	// Static variables
+	static const byte WAIT_IDENTIFIER = 0;
+	static const byte WAIT_LENGTH = 1;
+	static const byte WAIT_MESSAGE = 2;
+	// Regular variables
+	byte serialState;
+	byte identifier;
+	byte bytesExpected;
+	byte bytesRecieved;
+	char serialBuffer[50];
+	// The function to call when data received is complete
+	void (*parseData_callback)(byte, byte, void*);
+	
+	// Functions
+public:
+	Heartbeat_class();
+	~Heartbeat_class();
+	
+	void begin(void (*callback)(byte, byte, void*));
+	
+	void checkSerial();
+
+	void sendByte(const byte& id, const byte& data);
+	void sendInt(const byte& id, const int& data);
+	void sendString(const byte& id, const String& data);
+	
+	void sendHeartbeat();
+	void sendMonitor(String data);
+};
+
+extern Heartbeat_class Heartbeat;
+
+#endif
